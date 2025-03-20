dd88158cfb68c2724f8d0d333ef35dc98dab8525
rejo132 <enoch.luke1@student.moringaschool.com>
2025-03-20 23:17:25 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+// index.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const element = document.getElementById('text'); // Assuming the element has id="text"
+    element.textContent = 'This is really cool!';
+});
